e6d6c9481549bd6a928182a48c79f2d0ca19c535
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2017-02-20 13:24:45 -0800

    Initial Commit

---
+++ QuietModemKit/QuietModemKit.h
@@ -0,0 +1,19 @@
+//
+//  QuietModemKit.h
+//  QuietModemKit
+//
+//  Created by Brian Armstrong on 2/20/17.
+//  Copyright © 2017 Quiet Modem Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuietModemKit.
+FOUNDATION_EXPORT double QuietModemKitVersionNumber;
+
+//! Project version string for QuietModemKit.
+FOUNDATION_EXPORT const unsigned char QuietModemKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuietModemKit/PublicHeader.h>
+
+
---
+++ QuietModemKitTests/QuietModemKitTests.m
@@ -0,0 +1,39 @@
+//
+//  QuietModemKitTests.m
+//  QuietModemKitTests
+//
+//  Created by Brian Armstrong on 2/20/17.
+//  Copyright © 2017 Quiet Modem Project. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QuietModemKitTests : XCTestCase
+
+@end
+
+@implementation QuietModemKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
